d643863c99d0a10b8e33084d187ca269929680dd
AnielleLuiz <anielleluizcosta@gmail.com>
2022-05-15 19:31:40 -0300

    add correção etag s3_file

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket_object" "job_spark" {
   bucket = aws_s3_bucket.dl.id
   key    = "emr-code/pyspark/job_spark_from_tf.py"
   source = "../job_spark.py"
-  etag   = filemd5("C../job_spark.py")
+  etag   = filemd5("../job_spark.py")
 }
 
 resource "aws_s3_bucket_object" "delta_insert" {
