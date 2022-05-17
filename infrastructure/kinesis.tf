9f93559f3cc14094d68f74676c262b0c88cd260c
AnielleLuiz <anielleluizcosta@gmail.com>
2022-05-17 20:47:56 -0300

    corrige kinesis.tf

--- infrastructure/kinesis.tf
+++ infrastructure/kinesis.tf
@@ -18,23 +18,22 @@ resource "aws_kinesis_firehose_delivery_stream" "extended_s3_stream" {
    }
  }
 
-
 resource "aws_iam_role" "firehose_role" {
   name = "IGTI_firehose_role"
-  
-  assume_role_policy = <<EOF 
+
+  assume_role_policy = <<EOF
 {
-		"Version": "2012-10-17",
-		"Statement": [
-		  {
-			"Action": "sts:AssumeRole",
-			"Principal": {
-				"Service": "firehose.amazonaws.com"
-			},
-			"Effect": "Allow",
-			"Sid": ""
-		}
-	]
-} 
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+       "Principal": {
+        "Service": "firehose.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+ ]
+}
 EOF
 }
